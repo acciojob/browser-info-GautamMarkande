e3390ed3e042d265a3f384fc9a3d592b7e9b95f7
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-05 12:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+const browserName = navigator.userAgent;
+const version = navigator.appVersion;
+const divEle = document.createElement("div");
+divEle.id = "browser-info";
+divEle.innerText =  `You are using ${browserName} version ${version}`;
+document.body.appendChild(divEle);
